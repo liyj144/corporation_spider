d3e864e21aeccd2fab6ac892a7555b13a269f512
liyj144 <liyj144@163.com>
2016-09-02 15:39:57 +0800

    打开全量更新

--- corporation/misc/middlewares.py
+++ corporation/misc/middlewares.py
@@ -11,14 +11,14 @@ class IgnoreHttpRequestMiddleware(object):
     def process_response(self, request, response, spider):
         corp_id = request.meta.get('corp_id')
         # 获取上海的企业
-        province = request.meta.get('province')
-        area = request.meta.get('area')
-        if province and province in [u'河北', u'湖南', u'浙江', u'安徽', u'江苏']:
-            logging.debug("%s already scrapy, skipped" % province)
-            raise IgnoreRequest("%s no in such province" % province)
-        if province and province not in [u'上海', u'北京']:
-            logging.debug("%s no in such province, skipped" % province)
-            raise IgnoreRequest("%s no in such province" % province)
+        #province = request.meta.get('province')
+        #area = request.meta.get('area')
+        #if province and province in [u'河北', u'湖南', u'浙江', u'安徽', u'江苏']:
+        #    logging.debug("%s already scrapy, skipped" % province)
+        #    raise IgnoreRequest("%s no in such province" % province)
+        #if province and province not in [u'上海', u'北京']:
+        #    logging.debug("%s no in such province, skipped" % province)
+        #    raise IgnoreRequest("%s no in such province" % province)
         #if area and area not in [u'黄浦区']:
         #    logging.debug("%s no in such area, skipped" % area)
         #    raise IgnoreRequest("%s no in such area" % area)
